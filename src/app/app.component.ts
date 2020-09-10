6effea296957fb6fdd6581494336ed78b8c0ec4a
Elis Seiti <elis.seiti@rise-world.com>
2020-09-10 14:31:14 +0200

    file upload component creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FileService} from './file.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-new-app';
+  file: [];
+  imgArray: any;
+  imgTypes = ['jpg', 'jpeg', 'png', 'gif'];
+
+  constructor(fileService: FileService) {
+  }
+  handleUploadChange(event: any) {
+
+    event.preventDefault();
+    const files = event.target.files || [];
+    console.log(files);
+    for (let index = 0; index < files.length; index++) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        if (reader.readyState === 2) {
+          const extension = files[index].name.split('.').pop().toLowerCase();
+          if (this.imgTypes.indexOf(extension) > -1) {
+            this.imgArray.push(reader.result);
+          } else {
+            this.imgArray.push('');
+          }
+          this.file = this.imgArray;
+        }
+      };
+
+      reader.readAsDataURL(event.target.files[index]);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UploadComponent } from './upload/upload.component';
+import { ProgressComponent } from './progress/progress.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadComponent,
+    ProgressComponent
   ],
   imports: [
     BrowserModule
